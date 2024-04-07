af3917503742ff710cad1a44f36a935c07fb8d6c
Sufyan <bucks.pri@gmail.com>
2024-04-07 16:10:33 +0530

    manage cam added

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ from flask import Flask, render_template, Response,request,redirect
 
 app = Flask(__name__)
 
-cameras={"0":{
-    "region":[],
-    "flag_r_zone":True,
-    "flag_pose_alert":False,
-    "flag_fire":False,
-    "flag_gear":False
-}}
+cameras={}
 
 def process_frames(camid,region,flag_r_zone=False,flag_pose_alert=False,flag_fire=False,flag_gear=False):
     if  (len(camid)==1):
@@ -47,26 +41,37 @@ def manage_cam_page():
 @app.route('/video_feed/<int:cam_id>')
 def video_feed_generator(cam_id):
     camid=str(cam_id)
-    flag_r_zone=cameras[camid]["flag_r_zone"]
-    flag_pose_alert=cameras[camid]["flag_pose_alert"]
-    flag_fire=cameras[camid]["flag_fire"]
-    flag_gear=cameras[camid]["flag_gear"]
+    flag_r_zone=cameras[camid]["r_region"]
+    flag_pose_alert=cameras[camid]["pose"]
+    flag_fire=cameras[camid]["fire"]
+    flag_gear=cameras[camid]["gear"]
     region=cameras[camid]["region"]
 
     return Response(process_frames(camid,region,flag_r_zone,flag_pose_alert,
                                    flag_fire,flag_gear), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route("/get_cam_details",methods=['GET','POST'])
+def getting_cam_details():
+    if(request.method=='POST'):
+        camid=request.form['Cam_id']
+        
+        fire_bool= "fire" in request.form
+        pose_bool= "pose_alert" in request.form
+        r_bool= "R_zone" in request.form
+        s_gear_bool= "Safety_gear" in request.form
+
+    cameras[camid]={
+        "fire": fire_bool,
+        "pose": pose_bool,
+        "r_region": r_bool,
+        "gear": s_gear_bool,
+        "region": []
+    }
+        
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
-""""
-cam id: str
-fire: check button
-pose alert: check button
-restricted zone: check button
-safety gear: check button
-
-"""
